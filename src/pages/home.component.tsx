441deb0c01c0d98cd3e705ade2ec9e357c335926
Andres Munevar <afmunevarm@hotmail.com>
2021-10-17 13:58:27 -0400

    Remove clutter

--- src/pages/home.component.tsx
+++ src/pages/home.component.tsx
@@ -1,30 +1,14 @@
 import Calendar from "../components/calendar/calendar.component";
-import FormDropdown from "../components/form-dropdown/form-dropdown.component";
+import FormGroup from "../components/form-group/form-group.component";
 import SideMenu from "../components/side-menu/side-menu.component";
 
 const HomePage = () => {
-  const data: string[] = ["one", "two"];
-  //Temp home for this action:
-  // useSelector-> map of selections {program: '', starts on: '', campus: '', co-op: ''}
-  //In some 3 year programs, 3rd year courses might be offered only during one given semester a year
   return (
     <div>
-      {/* OPTIONS: must pass as array of strings
-        -programs
-        -starting term
-        -co-op
-      */}
+      {/* Create component to make a hidden side menu */}
       <SideMenu>
-        <FormDropdown
-          label="Program Name"
-          selectionOptions={data}
-        />
-        <FormDropdown
-          label="Starting Year"
-          selectionOptions={data}
-        />
+        <FormGroup />
       </SideMenu>
-      {/* Only render calendar when program set */}
 
       <Calendar />
     </div>
